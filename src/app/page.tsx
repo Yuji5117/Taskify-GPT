e28a1dc2deb5a0ed07a6419506c8e38f3058e544
Yuji Nakamura <ny5117@icloud.com>
2025-07-09 19:54:01 +0700

    implemet task extract button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+"use client";
+
+import { ChangeEvent, useState } from "react";
+
 export default function Home() {
+  const [chatText, setChatText] = useState<string>("");
+
+  const handleChatChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setChatText(e.target.value);
+  };
+  const handleExtractTasks = () => {
+    alert(chatText);
+  };
+
   return (
     <div>
       <main>
         <div>
-          <textarea className="border" />
+          <textarea
+            value={chatText}
+            onChange={handleChatChange}
+            className="border"
+            placeholder="ChatGPTとの会話を貼り付けてください"
+          />
+          <button onClick={handleExtractTasks}>タスクの抽出</button>
         </div>
       </main>
     </div>
